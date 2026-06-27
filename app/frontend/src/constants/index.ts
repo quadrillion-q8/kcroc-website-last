fc1457bdfa89928108ad4f3ef532d18b76721916
Kuwait Computer Repair on Call <quadrillion1980@gmail.com>
2026-06-27 16:05:04 +0300

    Create index.ts

---
+++ app/frontend/src/constants/index.ts
@@ -0,0 +1,22 @@
+// File: app/frontend/src/constants/index.ts
+
+// 1. Assets
+export * from './assets/images';
+
+// 2. Business Logic
+export * from './business/data';
+export * from './business/routes';
+
+// 3. Settings
+export * from './settings/seo';
+
+// 4. Content
+export * from './content/aiPagesData';
+export * from './content/areas';
+export * from './content/blogPosts';
+export * from './content/entities';
+export * from './content/faqs';
+export * from './content/galleryData';
+export * from './content/knowledgeBase';
+export * from './content/locationAreas';
+export * from './content/services';
