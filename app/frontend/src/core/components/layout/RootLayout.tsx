4e1739e7f712b490df5d00b081d75c4e88a35565
Kuwait Computer Repair on Call <quadrillion1980@gmail.com>
2026-07-02 02:30:00 +0300

    Update RootLayout.tsx

--- app/frontend/src/core/components/layout/RootLayout.tsx
+++ app/frontend/src/core/components/layout/RootLayout.tsx
@@ -1,15 +1,22 @@
 // File: app/frontend/src/core/components/layout/RootLayout.tsx
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Header } from './Header';
 import { Footer } from './Footer';
 
 export const RootLayout: React.FC = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-brand-dark text-foreground font-sans selection:bg-brand-primary/30">
-      <main className="flex-grow flex flex-col">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
+    
+      {/* Top Level Global Navigation */}
+      
+      
+      {/* Content Engine Outlet Viewport */}
+      
+        
+      
+
+      {/* Bottom Anchor Global Footer */}
+      
+    
   );
 };
