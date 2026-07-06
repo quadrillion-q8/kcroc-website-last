73dcd534609a11637d0323accabd11c21f7604cc
Kuwait Computer Repair on Call <quadrillion1980@gmail.com>
2026-07-06 03:51:46 +0300

    Update Home.tsx

--- app/frontend/src/pages/Home.tsx
+++ app/frontend/src/pages/Home.tsx
@@ -3,19 +3,25 @@ import React from 'react';
 import { SEOEngine } from '../core/components/SEOEngine';
 import Hero from '../components/home/Hero';
 import { TrustBar } from '../components/home/TrustBar';
+import { StatsRow } from '../components/home/StatsRow'; // New Component
 import ServicesGrid from '../components/home/ServicesGrid';
 import { Process } from '../components/home/Process';
+import FAQSection from '../components/home/FAQSection';
+import { ServiceAreas } from '../components/home/ServiceAreas'; // New Component
 import { ChatWidget } from '../components/ChatWidget';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#0a1628]"> {/* Matches body background */}
+    <main className="w-full min-h-screen bg-slate-950">
       <SEOEngine entityId="page-home" />
-      
+
       <Hero />
       <TrustBar />
+      <StatsRow />
       <ServicesGrid />
       <Process />
+      <FAQSection />
+      <ServiceAreas />
       
       <ChatWidget />
     </main>
