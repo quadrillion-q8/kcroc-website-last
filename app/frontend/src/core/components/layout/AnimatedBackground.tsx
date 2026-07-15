8b22fd6b6aa7dde517f8fe887e0b74ae643ec938
Kuwait Computer Repair on Call <quadrillion1980@gmail.com>
2026-07-15 05:19:02 +0300

    Update AnimatedBackground.tsx

--- app/frontend/src/core/components/layout/AnimatedBackground.tsx
+++ app/frontend/src/core/components/layout/AnimatedBackground.tsx
@@ -11,12 +11,15 @@ export const AnimatedBackground: React.FC = () => {
       {/* Tech Grid Overlay */}
       <div className="absolute inset-0 bg-[url('/grid.svg')] opacity-10" />
 
-      {/* The Final Glowing Orbs (Subtle, massive blur, slow animation) */}
-      <div className="absolute top-1/4 -left-10 w-[500px] h-[500px] rounded-full bg-cyan-500/10 blur-[120px] animate-blob" />
+      {/* 
+        Increased opacity (/30 and /20) and reduced blur slightly (100px) 
+        so the glowing orbs actually show up against the dark background! 
+      */}
+      <div className="absolute top-1/4 -left-10 w-[500px] h-[500px] rounded-full bg-cyan-500/30 blur-[100px] animate-blob" />
       
-      <div className="absolute bottom-1/4 -right-10 w-[500px] h-[500px] rounded-full bg-emerald-500/10 blur-[120px] animate-blob [animation-delay:3s]" />
+      <div className="absolute bottom-1/4 -right-10 w-[500px] h-[500px] rounded-full bg-emerald-500/20 blur-[100px] animate-blob [animation-delay:3s]" />
       
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] rounded-full bg-sky-500/10 blur-[150px] animate-blob [animation-delay:6s]" />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] rounded-full bg-sky-500/20 blur-[120px] animate-blob [animation-delay:6s]" />
 
     </div>
   );
